e8700ca5412ecc07abee406daa5fc9f95ba8d1ee
kushagrmishra <kushagr.mishra207@gmail.com>
2026-09-06 17:44:48 +0400

    feat: implement Lucas Bebber SVG gooey menu filters (shadowed-goo & goo) for navigation bar

--- App.tsx
+++ App.tsx
@@ -45,6 +45,8 @@ function GooeySvgFilter() {
     return (
         <>
             <svg
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
                 style={{
                     position: 'absolute',
                     width: 0,
@@ -56,33 +58,54 @@ function GooeySvgFilter() {
                 focusable="false"
             >
                 <defs>
-                    <filter id="gooey-nav" x="-20%" y="-20%" width="140%" height="140%">
-                        <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
-                        <feColorMatrix
-                            in="blur"
-                            mode="matrix"
-                            values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 20 -10"
-                            result="goo"
-                        />
-                        <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+                    {/* Lucas Bebber Shadowed Goo Filter */}
+                    <filter id="shadowed-goo">
+                        <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+                        <feGaussianBlur in="goo" stdDeviation="3" result="shadow" />
+                        <feColorMatrix in="shadow" mode="matrix" values="0 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 -0.2" result="shadow" />
+                        <feOffset in="shadow" dx="1" dy="1" result="shadow" />
+                        <feComposite in2="shadow" in="goo" result="goo" />
+                        <feComposite in2="goo" in="SourceGraphic" result="mix" />
                     </filter>
-                    <filter id="gooey-filter" x="-20%" y="-20%" width="140%" height="140%">
-                        <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
-                        <feColorMatrix
-                            in="blur"
-                            mode="matrix"
-                            values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 20 -10"
-                            result="goo"
-                        />
-                        <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+
+                    {/* Lucas Bebber Standard Goo Filter */}
+                    <filter id="goo">
+                        <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+                        <feComposite in2="goo" in="SourceGraphic" result="mix" />
+                    </filter>
+
+                    {/* Aliases for universal compatibility */}
+                    <filter id="gooey-nav">
+                        <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+                        <feGaussianBlur in="goo" stdDeviation="3" result="shadow" />
+                        <feColorMatrix in="shadow" mode="matrix" values="0 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 -0.2" result="shadow" />
+                        <feOffset in="shadow" dx="1" dy="1" result="shadow" />
+                        <feComposite in2="shadow" in="goo" result="goo" />
+                        <feComposite in2="goo" in="SourceGraphic" result="mix" />
+                    </filter>
+                    <filter id="gooey-filter">
+                        <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+                        <feComposite in2="goo" in="SourceGraphic" result="mix" />
                     </filter>
                 </defs>
             </svg>
             <style>
                 {`
+                    .gooey-shadowed-layer {
+                        filter: url(#shadowed-goo);
+                        -webkit-filter: url(#shadowed-goo);
+                    }
                     .gooey-nav-layer {
-                        filter: url(#gooey-nav);
-                        -webkit-filter: url(#gooey-nav);
+                        filter: url(#shadowed-goo);
+                        -webkit-filter: url(#shadowed-goo);
+                    }
+                    .gooey-layer {
+                        filter: url(#goo);
+                        -webkit-filter: url(#goo);
                     }
                 `}
             </style>
@@ -746,7 +769,7 @@ function MainApp() {
             <View
                 style={[
                     styles.navGooeyContainer,
-                    Platform.OS === 'web' ? ({ filter: 'url(#gooey-nav)', WebkitFilter: 'url(#gooey-nav)' } as any) : undefined,
+                    Platform.OS === 'web' ? ({ filter: 'url(#shadowed-goo)', WebkitFilter: 'url(#shadowed-goo)' } as any) : undefined,
                 ]}
                 pointerEvents="box-none"
             >
